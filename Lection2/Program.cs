9cefcf5d1bfebe09fb3d9e8c700d82030ce0deb0
Anna Chesnokova <chesnokova33@list.ru>
2023-07-10 21:31:50 +0300

    add Lection2

---
+++ Lection2/Program.cs
@@ -0,0 +1,63 @@
+﻿/*int[] array = { 1, 22, 32, 4, 52, 26, 4, 28 };
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index <= n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+*/
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
